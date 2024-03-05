941740fcafec296d7fa4fedc2ec3675ada9bd8fa
afnleaf <afnleafow@gmail.com>
2024-03-04 21:15:20 -0500

    Cleaned up some files

--- replaycode-ocr/run.py
+++ replaycode-ocr/run.py
@@ -4,7 +4,6 @@ from PIL import Image
 import cv2 as cv
 import numpy as np
 import re
-#from matplotlib import pyplot as plt
 
 
 def pre_process_input_image(image):
@@ -13,6 +12,7 @@ def pre_process_input_image(image):
     # thresholding
     #image = cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
     return image
+
 
 def process_cropped_image(image):
     # invert
@@ -37,6 +37,7 @@ def process_cropped_image(image):
 
     return image
 
+
 def process_text1(text):
     pattern = r'[^a-zA-Z0-9]+\s*(.*)'
 
@@ -49,6 +50,7 @@ def process_text1(text):
         print("Extracted chunk:", extracted_chunk)
     else:
         print("Pattern not found in the input string.")
+
 
 # process the string given by the tesseract engine
 def process_text(text):
@@ -74,84 +76,86 @@ def process_text(text):
     # return sliced text
     return text[start:end].upper()
 
+
 # print replay codes
 def print_codes(replaycodes):
     for code in replaycodes:
         print(code)
 
 
-# Load the image
-input_filename = 'image_proc4.jpg'
-#input_filename = 'result0.png'
-output_append = '/app/output/result'
-template_filename = 'template_proc.jpg'
-
-# load image
-img_rgb = cv.imread(input_filename)
-assert img_rgb is not None, "file could not be read, check with os.path.exists()"
-
-#img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-#img_bifilter = cv.bilateralFilter(img_gray,9,75,75)
-img_final = pre_process_input_image(img_rgb)
-cv.imwrite(output_append + "final.png", img_final)
-
-# load template
-template = cv.imread(template_filename, cv.IMREAD_GRAYSCALE)
-#template = cv.resize(template, (0,0), fx=1.5, fy=1.5) 
-assert template is not None, "file could not be read, check with os.path.exists()"
-# template_final = pre_process_image(template)
-w, h = template.shape[::-1]
-
-# match template to input
-res = cv.matchTemplate(img_final, template, cv.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where(res >= threshold)
-
-# list of replay codes
-replaycodes = []
-
-# rectangle around it
-outbound_w = 115
-outbound_h = 25
-i = 0
-for pt in zip(*loc[::-1]):
-    #cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), 2)
-    #cv.rectangle(img_rgb, pt, (pt[0] + outbound_wplus, pt[1] + outbound_hplus), (0,255,0), 2)
-    newptx = pt[0] - 2
-    newpty = pt[1] - 2
-    newpt = newptx, newpty
-    #cv.rectangle(img_rgb, newpt, (newpt[0] + outbound_w, newpt[1] + outbound_h), (0,255,0), 2)
-
-    # Crop the image around the contour
-    crop = img_final[newpt[1]:newpt[1] + outbound_h, newpt[0]:newpt[0] + outbound_w]
+def parse_image(input_filename, template_filename):
+    output_append = '/app/output/result'
     
-    # process cropped image
-    output_filename_before = output_append + "_before_" + str(i) + ".png"
-    # before
-    cv.imwrite(output_filename_before, crop)
-    crop_final = process_cropped_image(crop)
-    # after
-    output_filename_after = output_append + "_after_" + str(i) + ".png"
-    cv.imwrite(output_filename_after, crop_final)
+    # load image
+    img_rgb = cv.imread(input_filename)
+    assert img_rgb is not None, "file could not be read, check with os.path.exists()"
+
+    #img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+    #img_bifilter = cv.bilateralFilter(img_gray,9,75,75)
+    img_final = pre_process_input_image(img_rgb)
+    cv.imwrite(output_append + "final.png", img_final)
+
+    # load template
+    template = cv.imread(template_filename, cv.IMREAD_GRAYSCALE)
+    #template = cv.resize(template, (0,0), fx=1.5, fy=1.5) 
+    assert template is not None, "file could not be read, check with os.path.exists()"
+    # template_final = pre_process_image(template)
+    w, h = template.shape[::-1]
+
+    # match template to input
+    res = cv.matchTemplate(img_final, template, cv.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where(res >= threshold)
+
+    # list of replay codes
+    replaycodes = []
+
+    # rectangle around it
+    outbound_w = 115
+    outbound_h = 25
+    i = 0
+    for pt in zip(*loc[::-1]):
+        #cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), 2)
+        #cv.rectangle(img_rgb, pt, (pt[0] + outbound_wplus, pt[1] + outbound_hplus), (0,255,0), 2)
+        newptx = pt[0] - 2
+        newpty = pt[1] - 2
+        newpt = newptx, newpty
+        #cv.rectangle(img_rgb, newpt, (newpt[0] + outbound_w, newpt[1] + outbound_h), (0,255,0), 2)
+
+        # Crop the image around the contour
+        crop = img_final[newpt[1]:newpt[1] + outbound_h, newpt[0]:newpt[0] + outbound_w]
+        
+        # process cropped image
+        output_filename_before = output_append + "_before_" + str(i) + ".png"
+        # before
+        cv.imwrite(output_filename_before, crop)
+        crop_final = process_cropped_image(crop)
+        # after
+        output_filename_after = output_append + "_after_" + str(i) + ".png"
+        cv.imwrite(output_filename_after, crop_final)
 
 
-    # output code as text
-    text = pytesseract.image_to_string(crop_final)
-    #print(text)
-    #print(process_text(text))
-    code = process_text(text)
-    if code not in replaycodes:
-        replaycodes.append(code)
+        # output code as text
+        text = pytesseract.image_to_string(crop_final)
+        code = process_text(text)
+        # avoid duplicates
+        if code not in replaycodes:
+            replaycodes.append(code)
 
-    #data = pytesseract.image_to_data(crop_final, lang=None, config='', nice=0, output_type=Output.STRING, timeout=0, pandas_config=None)
-    #print(data)
+        i+=1
 
-    #boxes = pytesseract.image_to_boxes(crop_final)
-    #print(boxes)
+    # print all replay codes
+    print_codes(replaycodes)
+    
+    # print codes to file Todo
 
-    i+=1
+# main function
+def main():
+    input_filename = 'image_proc4.jpg'
+    template_filename = 'template_proc.jpg'
+    parse_image(input_filename, template_filename)
 
-# print all replay codes
-print_codes(replaycodes)
-
+# Default notation
+if __name__ == "__main__":
+    main()
 
