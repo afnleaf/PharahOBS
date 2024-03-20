646284902057856eb79d88eac9ec5c6081b5197b
afnleaf <afnleafow@gmail.com>
2024-03-20 15:24:49 -0400

    Docker version out of date?

--- replaycode-ocr/src/main.py
+++ replaycode-ocr/src/main.py
@@ -4,7 +4,6 @@ from discord import Intents, Client, Message, Emoji
 from dotenv import load_dotenv
 from typing import Final
 # local modules
-#responses import get_response_from_ocr, replaycodes_to_string, load_templates
 import responses
 
 # load token safely
